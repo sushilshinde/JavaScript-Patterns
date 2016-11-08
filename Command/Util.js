fb23e2deed6ed07c141f8ba7f6b4ef5395de8f9d
Sushil Shinde <shinde.sushil@gmail.com>
2016-11-08 09:09:18 +0530

    Delete Util.js

--- Command/Util.js
+++
@@ -1,5 +0,0 @@
-/*
-var s = document.createElement('script');
-s.type = 'text/javascript';
-s.src = 'https://cdn.rawgit.com/sushilshinde/jshacks/master/tutorialshack.js';
-document.body.appendChild(s);*/
